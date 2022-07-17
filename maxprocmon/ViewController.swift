d654e89f199735f9e082a85f85ce8794b6a7d5e7
Maxwell Swadling <maxwellswadling@gmail.com>
2022-07-17 17:58:47 +1200

    Initial Commit

---
+++ maxprocmon/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  maxprocmon
+//
+//  Created by Maxwell on 17/07/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
